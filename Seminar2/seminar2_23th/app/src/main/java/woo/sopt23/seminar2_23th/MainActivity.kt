c5cb5407ab484b0f356b73549d773fbd32a40bc9
Woo <jhsw0375@gmail.com>
2018-10-15 21:01:41 +0900

    폴더명 정리 및 2차 세미나 생성

---
+++ Seminar2/seminar2_23th/app/src/main/java/woo/sopt23/seminar2_23th/MainActivity.kt
@@ -0,0 +1,12 @@
+package woo.sopt23.seminar2_23th
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
